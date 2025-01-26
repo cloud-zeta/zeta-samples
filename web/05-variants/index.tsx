ce7c70faa8c1eeb816eb0e153a707d5379247f4f
Jiwen Cai <steve@cloudzeta.com>
2025-01-25 20:23:39 -0800

    Updaet OpenUSD variant web demo to use GetDefaultPrim

--- web/05-variants/index.tsx
+++ web/05-variants/index.tsx
@@ -1,14 +1,55 @@
-import { createRoot } from 'react-dom/client';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import * as React from 'react';
+import { createRoot } from 'react-dom/client';
 
 
 import { ZetaEngineBeta, ZetaEngineInit } from "@cloudzeta/engine";
-import { UsdVariantSet, UsdVariantSets } from "@cloudzeta/wasm";
+import { UsdPrim, UsdStage, UsdVariantSet, UsdVariantSets } from "@cloudzeta/wasm";
 
 const UsdVariantsDemo: React.FC = () => {
-    const [variantSetNames, setVariantSetNames] = React.useState<string[]>([]);
     const [variantSet, setVariantSet] = React.useState<UsdVariantSet>(null);
+    const [variantNames, setVariantNames] = React.useState<string[]>([]);
+
+    const loadFirstVariantSetFromDefaultPrim = async (stage: UsdStage): Promise<void> => {
+        const hasDefaultPrim = await stage.HasDefaultPrim();
+        if (!hasDefaultPrim) {
+            console.error("Stage has no default prim");
+            return;
+        }
+
+        const defaultPrim: UsdPrim = await stage.GetDefaultPrim();
+
+        if (!defaultPrim.IsValid()) {
+            console.error("Default prim is not valid");
+            return;
+        }
+
+        const variantSets: UsdVariantSets = defaultPrim.GetVariantSets();
+
+        // Iterate through all variant sets and get their names
+        const variantSetNames: string[] = [];
+        const names = variantSets.GetNames();
+        for (let i: number = 0; i < names.size(); i++) {
+            variantSetNames.push(names.get(i));
+        }
+
+        if (variantSetNames.length === 0) {
+            console.error("No variant sets found");
+            return;
+        }
+
+        // In this sample, we only use the first variant set.
+        const variantSet: UsdVariantSet = variantSets.GetVariantSet(variantSetNames[0]);
+
+        const variantNames = variantSet.GetVariantNames();
+        const variantNamesStr: string[] = [];
+        for (let i: number = 0; i < variantNames.size(); i++) {
+            variantNamesStr.push(variantNames.get(i));
+        }
+
+        setVariantSet(variantSet);
+        setVariantNames(variantNamesStr);
+    }
 
     React.useEffect(() => {
         const initializeEngine = async () => {
@@ -22,28 +63,10 @@ const UsdVariantsDemo: React.FC = () => {
 
             console.log("Engine created successfully");
 
-            engine.onSceneLoaded(() => {
+            engine.onSceneLoaded(async () => {
                 console.log("Scene loaded successfully");
 
-                const prim = engine.scene.GetStage()?.GetPrimAtPath("/vehicleVariant");
-                if (!prim) {
-                    console.error("Root prim not found");
-                    return;
-                }
-
-                const variantSets: UsdVariantSets = prim.GetVariantSets();
-                const variantSet: UsdVariantSet = variantSets.GetVariantSet("wheels");
-
-                const variantSetNames: string[] = [];
-                const names = variantSet.GetVariantNames();
-                for (let i: number = 0; i < names.size(); i++) {
-                    variantSetNames.push(names.get(i));
-                }
-
-                console.log("variantSetNames: ", variantSetNames);
-
-                setVariantSet(variantSet);
-                setVariantSetNames(variantSetNames);
+                loadFirstVariantSetFromDefaultPrim(engine.scene.GetStage());
             });
 
             // Load a scene from Cloud Zeta, the same scene can be viewed at:
@@ -58,12 +81,15 @@ const UsdVariantsDemo: React.FC = () => {
         <div style={{ position: "absolute", top: 16, left: 16 }}>
             <Menu as="div">
                 <MenuButton>
-                    Variants
+                {
+                    variantSet !== null &&
+                    variantSet.GetName()
+                }
                 </MenuButton>
 
                 <MenuItems>
                 {
-                    variantSetNames.map((name: string) => (
+                    variantNames.map((name: string) => (
                         <MenuItem key={name}>
                             <button onClick={ () => variantSet.SetVariantSelection(name) } >
                                 {name}
