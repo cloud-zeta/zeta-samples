10ff3b09b25db9ae4077fe8c94444a401eb25e16
Jiwen Cai <steve@cloudzeta.com>
2025-02-02 22:53:53 -0800

    Add sample for the editor

---
+++ web/06-editor/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path")
+const CopyPlugin = require("copy-webpack-plugin")
+const HtmlPlugin = require("html-webpack-plugin")
+
+module.exports = async () => {
+    return {
+        entry: "./index.ts",
+        output: {
+            path: path.resolve(__dirname, "public"),
+            filename: "index.js",
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, "node_modules/@cloudzeta/wasm"),
+                        to: path.join(__dirname, "public/zetaWasm"),
+                    },
+                    {
+                        from: path.resolve(__dirname, "node_modules/@cloudzeta/engine/zeta.worker.js"),
+                        to: path.join(__dirname, "public/zetaEngine/"),
+                    },
+                ]
+            }),
+            new HtmlPlugin({
+                template: path.resolve(__dirname, "index.html"),
+            }),
+        ],
+
+        resolve: {
+            extensions: [".js", ".ts"],
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.(ts)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        "ts-loader",
+                    ]
+                },
+            ]
+        },
+
+        devServer: {
+            compress: true,
+            port: 8080,
+
+            onBeforeSetupMiddleware: function (devServer) {
+                devServer.app.get("*", (req, res, next) => {
+                    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+                    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+                    res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+                    res.setHeader("Access-Control-Allow-Origin", "*");
+
+                    // Continue to the next middleware/handler
+                    next();
+                });
+            },
+        },
+    };
+}
